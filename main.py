54d1938b6062154c99b8d1b6d0068e1c64c7c838
Jia Yi <s709161616@gmail.com>
2025-08-07 00:24:24 +0800

    feat: 新增 flask_cors 支援跨來源請求

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 from api.books import books_bp
 from api.users import users_bp
+import re
+
 
 app = Flask(__name__)
+
+CORS(app, origins=[re.compile(r"http://localhost:\d+")])
+
 app.register_blueprint(books_bp)
 app.register_blueprint(users_bp)
 
